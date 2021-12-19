d73cd0a593bc7574d75c024d8e315f71a20c6690
alviansm <sandalproject7@gmail.com>
2021-12-19 18:30:05 +0700

    newscontext reworks

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -1,35 +1,37 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
-import { SearchContext } from "./components/Navbar";
 
 export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
-  const search = useContext(SearchContext);
+  const [search, setSearch] = useState();
   const apiKey = "0bc32f2370024b6eb068a5118c45ad8a";
-  console.log("search: ", search);
 
-  useEffect(() => {
-    if (search) {
-      axios
-      .get(
-        `https://newsapi.org/v2/everything?q=${search}&from=2021-10-21&sortBy=publishedAt&apiKey=${apiKey}`
-      )
-      .then((response) => setData(response.data))
-      .catch((error) => console.log(error));
-    } else {
-      axios
-      .get(
-        `https://newsapi.org/v2/everything?q=$obama&from=2021-10-21&sortBy=publishedAt&apiKey=${apiKey}`
-      )
-      .then((response) => setData(response.data))
-      .catch((error) => console.log(error))
-    }
-  }, []);
+  function getData(e) {
+    e.preventDefault();
+    console.log("button clicked.");
+    axios
+    .get(
+      `https://newsapi.org/v2/everything?q=${search}&from=2021-11-18&sortBy=publishedAt&apiKey=${apiKey}`)
+    .then((response) => setData(response.data))
+    .catch((error) => console.log(error));
+  }
 
   return (
     <NewsContext.Provider value={{ data }}>
+      <div>
+          <nav className="navbar navbar-light bg-light">
+          <div className="container">
+              <a className="navbar-brand">React NewsAPI</a>
+              <form className="d-flex">
+                <input className="form-control me-2" id="searchInput" type="search" placeholder="Search" aria-label="Search" onChange={event => setSearch(event.target.value)}/>
+                <button onClick={getData}>Search</button>
+              </form>
+          </div>
+          </nav>
+      </div>
+      <div>Search: {search}</div>    
       {props.children}
     </NewsContext.Provider>
   );
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,21 +1,19 @@
-import React, { useContext } from "react";
+import React, { createContext, useContext } from "react";
 import { NewsContext } from "../NewsContext";
 import Card from "./Card";
-import { SearchProvider } from "./Navbar";
 
 function News(props) {
   const { data } = useContext(NewsContext);
 
   return (
     <div>
-      <SearchProvider></SearchProvider>
       <div class="container">
         <div class="row">
             {data
               ? data.articles.map((news) => (
                   <Card data={news} key={news.url} />
                 ))
-              : "Loading..."}
+              : "Processing..."}
           </div>
       </div>
     </div>
